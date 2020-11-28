6e82a06424c521f8c2784e56248d788c47b99edb
Hardik Prajapati <hardikpnsp@gmail.com>
2020-11-28 12:03:43 +0530

    ADD: exercise 2.2

---
+++ chapter_02/exercise_2_2.scm
@@ -0,0 +1,30 @@
+(define make-segment cons)
+(define start-segment car)
+(define end-segment cdr)
+(define make-point cons)
+(define x-point car)
+(define y-point cdr)
+
+(define (midpoint-segment segment)
+  (make-point (/ (+ (x-point (start-segment segment))
+                    (x-point (end-segment segment)))
+                 2)
+              (/ (+ (y-point (start-segment segment))
+                    (y-point (end-segment segment)))
+                 2)))
+
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
+
+(define a (make-segment (make-point 1 2)
+                        (make-point 1 4)))
+
+(define m (midpoint-segment a))
+
+(print-point m)
+;; (1,3)
