9234599d57b67dd23d508d97c96ad40f699e63a9
Hardik Prajapati <hardikpnsp@gmail.com>
2020-12-05 16:34:34 +0530

    ADD: exercise 2.26

---
+++ chapter_02/exercise_2_26.scm
@@ -0,0 +1,14 @@
+;; import append
+(load "exercise_2_18.scm")
+
+(define x (list 1 2 3))
+(define y (list 4 5 6))
+
+(append x y)
+;; (1 2 3 4 5 6)
+
+(cons x y)
+;; ((1 2 3) 4 5 6)
+
+(list x y)
+;; ((1 2 3) (4 5 6))
