905612c0b24bab9ff30afe4a79635ed5f1e39b8c
Hardik Prajapati <hardikpnsp@gmail.com>
2020-12-05 10:37:38 +0530

    ADD: exercise 2.22

---
+++ chapter_02/exercise_2_22.scm
@@ -0,0 +1,25 @@
+(define (square-list items)
+  (define (iter things answer)
+    (if (null? things)
+        answer
+        (iter (cdr things)
+              (cons (square (car things))
+                    answer))))
+  (iter items '()))
+
+(square-list (list 1 2 3 4 5))
+;; get answer in reverse
+
+(define (square-list items)
+  (define (iter things answer)
+    (if (null? things)
+        answer
+        (iter (cdr things)
+              (cons answer
+                    (square (car things))))))
+  (iter items '()))
+
+(square-list (list 1 2 3 4 5))
+;; no-longer a list as nil element is at the start and it just becomes a nested cons
+
+;; one solutino can be -> get the answer in reverse and then reverse the list
