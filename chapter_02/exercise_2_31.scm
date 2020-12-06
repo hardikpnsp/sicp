bf4a9024878b4b88fddffd29f37d7e9fa13d99f3
Hardik Prajapati <hardikpnsp@gmail.com>
2020-12-06 10:44:05 +0530

    ADD: exercise 2.31

---
+++ chapter_02/exercise_2_31.scm
@@ -0,0 +1,12 @@
+(define (square-tree tree) (tree-map square tree))
+
+(define (tree-map func tree)
+  (map (lambda (sub-tree) (cond ((pair? sub-tree) (tree-map func sub-tree))
+                                (else (func sub-tree))))
+       tree))
+
+
+(square-tree
+ (list 1
+       (list 2 (list 3 4) 5)
+       (list 6 7)))
