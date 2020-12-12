75229c3496afeb0d12e7d2d37f80ad6421a007b1
Hardik Prajapati <hardikpnsp@gmail.com>
2020-12-12 10:53:35 +0530

    ADD: exercise 2.50

---
+++ chapter_02/exercise_2_50.scm
@@ -0,0 +1,17 @@
+(define (flip-horiz painter)
+  (transform-painter painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
+
+(define (rotate-counter-180 painter)
+  (transform-painter painter
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 1.0)
+                     (make-vect 1.0 0.0)))
+
+(define (rotate-counter-270 painter)
+  (transform-painter painter
+                     (make-vect 0.0 1.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
