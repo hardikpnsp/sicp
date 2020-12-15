a8997de1a08bc5c66e09e6b5b0a32a5786b6b2f5
Hardik Prajapati <hardikpnsp@gmail.com>
2020-12-15 20:36:45 +0530

    ADD: exercise 2.61

--- chapter_02/2_3_3_representing_sets.scm
+++ chapter_02/2_3_3_representing_sets.scm
@@ -1,3 +1,5 @@
+#| -> Sets as unordered lists |#
+
 (define (element-of-set? x set)
   (cond ((null? set) false)
         ((equal? x (car set)) true)
@@ -42,3 +44,40 @@
         (else (intersection-set (cdr set1) set2))))
 
 ;; would use this representations where additive procedures are important compared to intersection
+
+#| -> Sets as ordered lists |#
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((= x (car set)) true)
+        ((< x (car set)) false)
+        (else (element-of-set? x (cdr set)))))
+
+
+(define (intersection-set set1 set2)
+  (if (or (null? set1) (null? set2))
+      '()
+      (let ((x1 (car set1))
+            (x2 (car set2)))
+        (cond ((= x1 x2)
+               (cons x1
+                     (intersection-set (cdr set1)
+                                       (cdr set2))))
+              ((< x1 x2)
+               (intersection-set (cdr set1) set2))
+              ((< x2 x1)
+               (intersection-set set1 (cdr set2)))))))
+
+;; Exercise 2.61: adjoin-set
+
+(define (adjoin-set x set)
+  (cond ((null? set) (cons x '()))
+        ((= x (car set)) (cdr set))
+        ((< x (car set)) (cons x set))
+        (else (cons (car set)
+                    (adjoin-set x (cdr set))))))
+
+(adjoin-set 3 (list 1 2 4 5 6))
+
+;; here adjoin-set only has to iterate till x < (car set).
+;; it is on average about half compared to element-of-set? 
