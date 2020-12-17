a4f922e13b5f154adb324006be9f0e018a499221
Hardik Prajapati <hardikpnsp@gmail.com>
2020-12-17 10:19:31 +0530

    ADD: exercise 2.64

--- chapter_02/2_3_3_representing_sets.scm
+++ chapter_02/2_3_3_representing_sets.scm
@@ -185,3 +185,65 @@ Do the two procedures have the same order of growth? NO
 -> 2 has O(N) complexity 
 
 |#
+
+;; Exercise 2.64: list->tree
+
+(define (list->tree elements)
+  (car (partial-tree elements (length elements))))
+
+(define (partial-tree elts n)
+  (if (= n 0)
+      (cons '() elts)
+      (let ((left-size (quotient (- n 1) 2)))
+        (let ((left-result (partial-tree elts left-size)))
+          (let ((left-tree (car left-result))
+                (non-left-elts (cdr left-result))
+                (right-size (- n (+ left-size 1))))
+            (let ((this-entry (car non-left-elts))
+                  (right-result (partial-tree (cdr non-left-elts) right-size)))
+              (let ((right-tree (car right-result))
+                    (remaining-elts (cdr right-result)))
+                (cons (make-tree this-entry left-tree right-tree)
+                      remaining-elts))))))))
+
+(list->tree (list 1 3 5 7 9 11))
+
+#| Output
+
+        5
+     /     \
+    1       9
+     \     / \
+      3   7   11
+
+
+
+|#
+
+(list->tree (list 1 10 2 9 3 8))
+
+
+#| Output
+
+        2
+     /     \
+    1       3
+     \     / \
+      10  9   8
+
+|#
+
+
+#| 
+
+How does it work? 
+
+At each step it devides the list into three parts: left tree, this entry and right tree.
+the division is done by a number: left-size or right-size instead of actual division.
+the recursive calls ultimately give a balaned binary tree (without caring about the relations between numbers.
+
+Time complexity: O(N)
+
+At each step there are N/2 + N/2 = N items processed.
+
+|#
