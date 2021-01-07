9046b5da5af35e5c5dcaacf3f16b4f11241aae2e
Hardik Prajapati <hardikpnsp@gmail.com>
2021-01-07 09:05:03 +0530

    ADD: exercise 2.85

--- chapter_02/2_5_2_combining_data_of_different_types.scm
+++ chapter_02/2_5_2_combining_data_of_different_types.scm
@@ -141,3 +141,89 @@ trace:
 (put 'raise 'integer (lambda (x) (make-rational x 1))) 
 (put 'raise 'rational (lambda (x) (make-real (/ (numer x) (denom x)))))  
 (put 'raise 'real (lambda (x) (make-from-real-imag x 0))) 
+
+;; Exercise 2.84: apply-generic with raises (copied from sicpwiki)
+
+(define (level type) 
+(cond ((eq? type 'integer) 0) 
+      ((eq? type 'rational) 1) 
+      ((eq? type 'real) 2) 
+      ((eq? type 'complex) 3) 
+      (else (error "Invalid type: LEVEL" type)))) 
+
+(define (apply-generic op . args) 
+(let ((type-tags (map type-tag args))) 
+  (define (no-method) 
+    (error "No method for these types" (list op type-tags))) 
+  (let ((proc (get op type-tags))) 
+    (if proc 
+        (apply proc (map contents args)) 
+        (if (not (null? (cdr args))) 
+            (let ((raised-args (raise-to-common args))) 
+              (if raised-args 
+                  (let ((proc (get op (map type-tag raised-args)))) 
+                    (if proc 
+                        (apply proc (map contents raised-args)) 
+                        (no-method))) 
+                  (no-method))) 
+            (no-method)))))) 
+
+(define (raise-to-common args) 
+(let ((raised-args 
+       (map (lambda (x) (raise-to-type (highest-type args) x)) 
+            args))) 
+  (if (all-true? raised-args) 
+      raised-args 
+      false))) 
+
+(define (all-true? lst) 
+(cond ((null? lst) true) 
+      ((car lst) (all-true? (cdr lst))) 
+      (else false))) 
+
+(define (raise-to-type type item)
+(let ((item-type (type-tag item))) 
+  (if (eq? item-type type) 
+      item 
+      (let ((raise-fn (get 'raise item-type))) 
+        (if raise-fn 
+            (raise-to-type type (raise-fn item)) 
+            false)))))
+
+(define (highest-type args) 
+(if (null? (cdr args)) 
+    (type-tag (car args)) 
+    (let ((t1 (type-tag (car args))) 
+          (t2 (highest-type (cdr args)))) 
+      (let ((l1 (level t1)) (l2 (level t2))) 
+        (if (> l1 l2) t1 t2)))))
+
+;; Exercise: 2.85: drop (copied from sicp wiki)
+
+(put 'project 'rational 
+     (lambda (x) (make-scheme-number (round (/ (numer x) (denom x)))))) 
+
+;; add into real package 
+(put 'project 'real 
+     (lambda (x)  
+       (let ((rat (rationalize  
+                   (inexact->exact x) 1/100))) 
+         (make-rational 
+          (numerator rat) 
+            (denominator rat))))) 
+
+;; add into complex package 
+(put 'project 'complex 
+     (lambda (x) (make-real (real-part x)))) 
+
+(define (drop x) 
+  (let ((project-proc (get 'project (type-tag x)))) 
+    (if project-proc 
+        (let ((project-number (project-proc (contents x)))) 
+          (if (equ? project-number (raise project-number)) 
+              (drop project-number) 
+              x)) 
+        x))) 
+
+;; apply-generic   
+(drop (apply proc (map contents args))) 
