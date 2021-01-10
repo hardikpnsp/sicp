128d75568179cb2e41faa002a9da52ceef2e794c
Hardik Prajapati <hardikpnsp@gmail.com>
2021-01-10 12:20:29 +0530

    ADD: exercise 2.91

--- chapter_02/2_5_3_symbolic_algebra.scm
+++ chapter_02/2_5_3_symbolic_algebra.scm
@@ -132,3 +132,35 @@ and store different implementations for adjoin, first-term
 and define generic operation using apply-generic over sparse-term-list and dense-term-list
 
 |#
+
+;; Exercise 2.91: division
+
+(define (div-terms L1 L2)
+  (if (empty-termlist? L1)
+      (list (the-empty-termlist) (the-empty-termlist))
+      (let ((t1 (first-term L1))
+            (t2 (first-term L2)))
+        (if (> (order t2) (order t1))
+            (list (the-empty-termlist) L1)
+            (let ((new-c (div (coeff t1) (coeff t2)))
+                  (new-o (- (order t1) (order t2))))
+              (let ((rest-of-result
+                     (div-terms (sub-poly L1
+                                          (mul-poly L2
+                                                    (make-poly (0
+                                                                new-c))
+                                                    ))
+                                L2))))
+              (list (adjoin-term (make-term new-o
+                                            new-c)
+                                 (car rest-of-result))
+                    (cadr rest-of-result)))))))
+
+(define (div-poly p1 p2)
+  (if (same-variable? (variable p1) (variable p2))
+      (let ((result (div-terms (termlist p1) (termlist p2))))
+        (list (make-poly (variable p1)
+                         (car result))
+              (make-poly (variable p2)
+                         (cadr result))))
+      (error "poly not in same variable")))
