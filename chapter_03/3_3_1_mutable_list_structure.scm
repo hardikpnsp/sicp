28563e8eb991bb365c6077c9bd80a43e2645f256
Hardik Prajapati <hardikpnsp@gmail.com>
2021-01-21 23:04:01 +0530

    ADD: Exercise 3.12

---
+++ chapter_03/3_3_1_mutable_list_structure.scm
@@ -0,0 +1,43 @@
+;; 3.3 Modeling with Mutable Data
+
+;; 3.3.1 Mutable List Structure
+
+;; Exercise 3.12: append!
+
+(define x (list 'a 'b))
+(define y (list 'c 'd))
+
+(define z (append x y))
+
+(display z)
+;; (a b c d)
+
+(cdr x)
+;; (b)
+
+(define w (append! x y))
+
+(display w)
+;;(a b c d)
+
+(cdr x)
+;; (b c d)
+
+#| 
+
+1) append does not modify x
+
+     
+x -> |a||*| -> |b||'()|
+y -> |c||*| -> |d||'()| 
+
+z -> |a||*| -> |b||*| -> |c||*| -> |d||'()|
+
+2) append! modifies x
+
+x -> |a||*| -> |b||*|-> y
+
+y -> |c||*| -> |d||'()|
+
+w -> x
+|# 
