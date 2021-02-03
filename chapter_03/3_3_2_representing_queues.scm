ce3dc062fa76677b49e149d96667855c9972b55b
Hardik Prajapati <hardikpnsp@gmail.com>
2021-02-03 10:52:29 +0530

    ADD: Exercise 3.22

--- chapter_03/3_3_2_representing_queues.scm
+++ chapter_03/3_3_2_representing_queues.scm
@@ -62,3 +62,69 @@ if we add something else -> it will go through the empty-queue? flow in insert-q
   (display (front-ptr q)))
 
 (print-queue q1)
+
+
+
+;; Exercise 3.22: queue as procedure with local state
+
+(define (make-queue)
+  (let ((front-ptr '())
+        (rear-ptr '()))
+
+    (define (empty-queue?)
+      (null? front-ptr))
+    (define (front-queue)
+      (if (empty-queue?)
+          (error "FRONT called with an empty queue" front-ptr)
+          (car front-ptr)))
+
+    (define (set-front-ptr! item)
+      (set! front-ptr item))
+    (define (set-rear-ptr! item)
+      (set! rear-ptr item))
+    
+    (define (insert-queue! item)
+      (let ((new-pair (cons item '())))
+        (cond ((empty-queue?)
+               (set-front-ptr! new-pair)
+               (set-rear-ptr! new-pair)
+               front-ptr)
+              (else (set-cdr! rear-ptr new-pair)
+                    (set-rear-ptr! new-pair)
+                    front-ptr))))
+
+    (define (delete-queue!)
+      (cond ((empty-queue?)
+             (error "DELETE called with an empty queue"))
+            (else (set-front-ptr! (cdr front-ptr))
+                  front-ptr)))
+    
+    (define (dispatch m)
+      (cond ((eq? m 'front)
+             (front-queue))
+            ((eq? m 'insert!)
+             insert-queue!)
+            ((eq? m 'delete!)
+             (delete-queue!))
+            ((eq? m 'get)
+             front-ptr)
+            (else (error "Unsupported operation --DISPATCH" m))))
+    
+    dispatch))
+
+(define q (make-queue))
+
+((q 'insert!) 'a)
+;; (a)
+((q 'insert!) 'b)
+;; (a b)
+((q 'insert!) 'c)
+;; (a b c)
+(q 'front)
+;; a
+(q 'get)
+;; (a b c)  
+(q 'delete!)
+;; (b c)
+(q 'get)
+;; (b c)
