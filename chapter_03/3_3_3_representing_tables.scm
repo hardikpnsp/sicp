b30e1d7e0af6eef267fa7552f8e4e9d01d18370e
Hardik Prajapati <hardikpnsp@gmail.com>
2021-02-10 00:10:23 +0530

    ADD: Exercise 3.25

--- chapter_03/3_3_3_representing_tables.scm
+++ chapter_03/3_3_3_representing_tables.scm
@@ -46,3 +46,68 @@
 ((t 'lookup-proc) 'computer '+)
 ((t 'lookup-proc) 'math '-)
 
+;; Exercise 3.25: n dimension table
+
+;; returns record / subtable from a key and table of records
+(define (assoc key records)
+  ;;(display key)
+  ;;(newline)
+  ;;(display records)
+  ;;(newline)
+  (cond ((null? records) false)
+        ((equal? key (caar records)) records)
+        (else (assoc key (cdr records)))))
+
+
+;; recursive lookups
+(define (lookup key-list table)
+  (let ((key (car key-list))
+        (next-keys (cdr key-list)))
+    (let ((subtable (assoc key table)))
+      (if subtable
+          (cond ((null? next-keys) (cdr (car subtable)))
+                (else (lookup next-keys (cdr (car subtable)))))
+          false))))
+
+(define (insert! key-list value table)
+  (let ((key (car key-list))
+        (next-keys (cdr key-list)))
+    (let ((record (assoc key table)))
+      (if record
+          (cond ((null? next-keys)
+                 (display record)
+                 (set-cdr! (car record) value))
+                (else
+                 (display record)
+                 (newline)
+                 (set-cdr! (car record)
+                           (cons (cons key (make-table)) (cdr (car record))))
+                 (insert! next-keys value (cdr (car record)))))
+          (cond ((null? next-keys)
+                 (set-cdr! table
+                           (cons (cons key value) (cdr table))))
+                (else (set-cdr! table
+                                (cons (cons key (make-table)) (cdr table)))
+                      (insert! next-keys value (cdr (car (cdr table))))))))))
+
+(define (make-table) (cons (cons '*table '()) '()))
+
+(define t (make-table))
+
+(display t)
+
+(insert! (list 'foo) 'bar t)
+
+(insert! (list 'bar) 'foo t)
+
+(insert! (list 'f 'f) 'b t)
+
+(insert! (list 'foo 'foo) 'bar t)
+
+(insert! (list 'f 'f 'f) 'b t)
+
+(lookup (list 'foo) t)
+
+(lookup (list 'foo 'foo) t)
+
+(lookup (list 'f 'f 'f) t)
