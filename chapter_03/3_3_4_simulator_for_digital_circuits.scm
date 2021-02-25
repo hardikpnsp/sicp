2db35a36ed488a284a7b5f96fc30eabc1c72c03d
Hardik Prajapati <hardikpnsp@gmail.com>
2021-02-25 21:22:16 +0530

    ADD: Exercise 3.29

--- chapter_03/3_3_4_simulator_for_digital_circuits.scm
+++ chapter_03/3_3_4_simulator_for_digital_circuits.scm
@@ -12,3 +12,24 @@
   (add-action! a1 or-action)
   (add-action! a2 or-action)
   'ok)
+
+;; Exercise 3.29: OR gate with AND and NOT
+
+(define (or-gate a1 a2 output)
+  (let ((aa1 (make-wire))
+        (aa2 (make-wire))
+        (naa1 (make-wire))
+        (naa2 (make-wire))
+        (anaa (make-wire)))
+    (and-gate a1 a1 aa1)
+    (and-gate a2 a2 aa2)
+    (inverter aa1 naa1)
+    (inverter aa2 naa2)
+    (and-gate naa1 naa2 anaa)
+    (inverter anaa output)))
+
+#| The delay in this OR gate implementation is higher as it adds 
+
+2x (delay AND) + 2x (delay NOT)
+
+|#
