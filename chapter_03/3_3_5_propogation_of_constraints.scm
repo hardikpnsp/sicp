a1031e074da8ac8c13341931d1b28ec3d7608c5e
Hardik Prajapati <hardikpnsp@gmail.com>
2021-03-05 22:03:14 +0530

    ADD: Exercise 3.35

--- chapter_03/3_3_5_propogation_of_constraints.scm
+++ chapter_03/3_3_5_propogation_of_constraints.scm
@@ -185,3 +185,43 @@
 #| 
 The issue here is that if b is known, we can't find a as both a will be un-set
 |#
+
+
+;; Exercise 3.35: squarer
+
+(define (squarer a b)
+  (define (process-new-value)
+    (if (has-value? b)
+        (if (< (get-value b) 0)
+            (error "square less than 0 -- SQUARER" (get-value b))
+            (set-value! a (sqrt (get-value b)) me))
+        (if (has-value? a)
+            (let ((a-value (get-value a)))
+              (set-value! b (* a-value a-value) me)))))
+  
+  (define (process-forget-value)
+    (forget-value! a me)
+    (forget-value! b me)
+    (process-new-value))
+  
+  (define (me request)
+    (cond ((eq? request 'I-have-a-value)
+           (process-new-value))
+          ((eq? request 'I-lost-my-value)
+           (process-forget-value))
+          (else
+           (error "Unknown request --SQUARER"))))
+  (connect a me)
+  (connect b me)
+  me)
+
+(define a (make-connector))
+(define b (make-connector))
+
+(probe "a" a)
+(probe "b" b)
+
+(squarer a b)
+
+(set-value! a 10 'user)
+
