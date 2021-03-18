f53f244761389b8c59f93c842c7904634a3168d4
Hardik Prajapati <hardikpnsp@gmail.com>
2021-03-18 21:57:11 +0530

    ADD: Exercise 3.41 and 3.42

--- chapter_03/3_4_2_mechanisms_for_controlling_concurrency.scm
+++ chapter_03/3_4_2_mechanisms_for_controlling_concurrency.scm
@@ -24,6 +24,7 @@
                   (s (lambda () (set! x (+ x 1)))))
 ;; 101, 121, 100, 11
 
+
 ;; Exercise 3.40: Give all possible values of x
 
 (define x 10)
@@ -39,3 +40,55 @@
 (parallel-execute (s (lambda () (set! x (* x x))))
                   (s (lambda () (set! x (* x x x)))))
 ;; 1000000
+
+
+;; Exercise 3.41: Serialized balance
+
+(define (make-account balance)
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  ;; continued on next page
+
+  (let ((protected (make-serializer)))
+    (define (dispatch m)
+      (cond ((eq? m 'withdraw) (protected withdraw))
+            ((eq? m 'deposit) (protected deposit))
+            ((eq? m 'balance)
+             ((protected (lambda () balance)))) ; serialized
+            (else (error "Unknown request -- MAKE-ACCOUNT"
+                         m))))
+    dispatch))
+
+#| 
+- Reading will not be anomalous.
+|#
+
+;; Exercise 3.42: Reusing serialized methods, any differnece?
+
+(define (make-account balance)
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (let ((protected (make-serializer)))
+    (let ((protected-withdraw (protected withdraw))
+          (protected-deposit (protected deposit)))
+      (define (dispatch m)
+        (cond ((eq? m 'withdraw) protected-withdraw)
+              ((eq? m 'deposit) protected-deposit)
+              ((eq? m 'balance) balance)
+              (else (error "Unknown request -- MAKE-ACCOUNT"
+                           m))))
+      dispatch)))
+
+;; No difference in concurrency
