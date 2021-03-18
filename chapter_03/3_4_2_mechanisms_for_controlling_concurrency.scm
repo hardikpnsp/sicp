77824ee69a6c1926a7c88d417f8bcc4d74ddd071
Hardik Prajapati <hardikpnsp@gmail.com>
2021-03-18 21:15:30 +0530

    ADD: Exercise 3.39

---
+++ chapter_03/3_4_2_mechanisms_for_controlling_concurrency.scm
@@ -0,0 +1,25 @@
+;; 3.4.2 Mechanisms for Controlling Concurrency
+
+;; Exercise 3.39: Possible values for different serializers
+
+(define x 10)
+
+(parallel-execute (lambda () (set! x (* x x)))
+                  (lambda () (set! x (+ x 1))))
+;; 100, 11, 121, 101, 110
+
+(define x 10)
+
+(define s (make-serializer))
+
+(parallel-execute (s (lambda () (set! x (* x x))))
+                  (s (lambda () (set! x (+ x 1)))))
+;; 101 or 121
+
+(define x 10)
+
+(define s (make-serializer))
+
+(parallel-execute (lambda () (set! x ((s (lambda () (* x x))))))
+                  (s (lambda () (set! x (+ x 1)))))
+;; 101, 121, 100, 11
