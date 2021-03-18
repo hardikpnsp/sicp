bc80d5d85cc7668b813ee13563df1de7db81c1ad
Hardik Prajapati <hardikpnsp@gmail.com>
2021-03-18 21:19:24 +0530

    ADD: Exercise 3.40

--- chapter_03/3_4_2_mechanisms_for_controlling_concurrency.scm
+++ chapter_03/3_4_2_mechanisms_for_controlling_concurrency.scm
@@ -23,3 +23,19 @@
 (parallel-execute (lambda () (set! x ((s (lambda () (* x x))))))
                   (s (lambda () (set! x (+ x 1)))))
 ;; 101, 121, 100, 11
+
+;; Exercise 3.40: Give all possible values of x
+
+(define x 10)
+
+(parallel-execute (lambda () (set! x (* x x)))
+                  (lambda () (set! x (* x x x))))
+;; 100, 1000, 10000, 100000, 1000000
+
+(define x 10)
+
+(define s (make-serializer))
+
+(parallel-execute (s (lambda () (set! x (* x x))))
+                  (s (lambda () (set! x (* x x x)))))
+;; 1000000
