07295881f1f0f9cbd60001bb8a5c28574a9c13bb
Hardik Prajapati <hardikpnsp@gmail.com>
2021-03-20 12:40:14 +0530

    ADD: Exercise 3.43

--- chapter_03/3_4_2_mechanisms_for_controlling_concurrency.scm
+++ chapter_03/3_4_2_mechanisms_for_controlling_concurrency.scm
@@ -92,3 +92,81 @@
       dispatch)))
 
 ;; No difference in concurrency
+
+
+;; Exercise 3.43: exchanges
+
+;; First version
+(define (exchange account1 account2)
+  (let ((difference (- (account1 'balance)
+                       (account2 'balance))))
+    ((account1 'withdraw) difference)
+    ((account2 'deposit) difference)))
+
+;; Second version - Exposes bank account serializers using message passing
+(define (make-account-and-serializer balance)
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (let ((balance-serializer (make-serializer)))
+    (define (dispatch m)
+      (cond ((eq? m 'withdraw) withdraw)
+            ((eq? m 'deposit) deposit)
+            ((eq? m 'balance) balance)
+            ((eq? m 'serializer) balance-serializer)
+            (else (error "Unknown request -- MAKE-ACCOUNT"
+                         m))))
+    dispatch))
+
+(define (serialized-exchange account1 account2)
+  (let ((serializer1 (account1 'serializer))
+        (serializer2 (account2 'serializer)))
+    ((serializer1 (serializer2 exchange))
+     account1
+     account2)))
+
+#| 
+Three accounts a1, a2, a3 -> 10, 20, 30 balance
+If exchage porcesses are run sequencially, at any point on two accounts are "exchanged".
+This results in at any time, all three account having 10, 20 and 30 balance in any order
+
+In the first case -> Individual accounts are serialized but not the exchange process. Folling can happen
+
+A1         A2         A3   Description
+
+10         20         30   Initial config
+
+10         20    W <- 20   Exchange A2, A3 (W-A3-10) [E1]
+
+10    W <- 10         20   Exchange A1, A2 (W-A2-10) [E2]
+
+10         10    W <- 10   Exchange A1, A3 (W-A3-10) [E3]
+
+10         20 <- D    10   Exchange A2, A3 (D-A2-10) [E1-complete]
+
+10    W <- 10         10   Exchange A1, A2 (W-A2-10) [E4]
+
+20 <- D    10         10   Exchange A1, A2 (D-A1-10) [E2-complete]
+
+30 <- D    10         10   Exchange A1, A3 (D-A1-10) [E3-complete]
+
+40 <- D    10         10   Exchange A1, A2 (D-A2-10) [E4-complete]
+
+
+Thus, the overall sum will always remain same (60) but the rules of exchanges can be broken.
+
+If transactions are not serialized, this condition gets violated as well.
+
+A1 A2 A3
+10 20 30 
+10 20 0  Exchange A1, A3 and Exchange A2, A3 (W-A3-10, W-A3-20)
+10 0  0  Exchange A2, A3 and Exchange A2, A3 (W-A2-20, D-A2-10)
+
+Here both Deposit and Withdraw happen in an interleaved manner and A2 is set to 0 -> $10 deposit are lost
+
+|#
