1f7e5dd5d09439346bb3d33953910b4ed50ac040
Hardik Prajapati <hardikpnsp@gmail.com>
2021-05-01 10:30:30 +0530

    ADD: Exercise 3.51

--- chapter_03/3_5_1_streams_are_delayed_lists.scm
+++ chapter_03/3_5_1_streams_are_delayed_lists.scm
@@ -10,3 +10,33 @@
        (apply proc (map stream-car argstreams))
        (apply stream-map
               (cons proc (map stream-cdr argstreams))))))
+
+;; Exercise 3.51: what will it print
+
+(define (stream-enumerate-interval low high)
+  (if (> low high)
+      the-empty-stream
+      (cons-stream
+       low
+       (stream-enumerate-interval (+ low 1) high))))
+
+(define (display-stream s)
+  (stream-for-each display-line s))
+
+(define (display-line x)
+  (newline)
+  (display x))
+
+(define (show x)
+  (display-line x)
+  x)
+
+(define x (stream-map show (stream-enumerate-interval 0 10)))
+(stream-ref x 5)
+;; 1 2 3 4 5
+;; Stream is evaluated till 5
+
+(stream-ref x 7)
+;; 6 7
+;; stream is already evaluated till 5 so no show calls till that
+;; 6 and 7 are not evaluated -> show is called on them
