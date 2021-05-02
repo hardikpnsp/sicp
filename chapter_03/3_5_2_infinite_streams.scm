b8ba40eb70f65083d78ce394d76e62a368f3a2a9
Hardik Prajapati <hardikpnsp@gmail.com>
2021-05-02 10:44:20 +0530

    ADD: Exercise 3.54

--- chapter_03/3_5_2_infinite_streams.scm
+++ chapter_03/3_5_2_infinite_streams.scm
@@ -7,3 +7,17 @@
 
 (define s (cons-stream 1 (add-stream s s)))
 ;; 1 2 4 8 16 32 ...
+
+;; Exercise 3.54: mul-stream
+
+(define (mul-stream s1 s2)
+  (stream-map * s1 s2))
+
+(define ones (cons-stream 1 ones))
+
+(define integers (cons-stream 1 (add-stream ones integers)))
+
+;; (n + 1)! = (n!) * (n + 1)
+(define factorials (cons-stream 1 (mul-stream factorials
+                                              integers)))
+
