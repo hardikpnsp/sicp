31b468cf870fc51b8ece4e84a295bb8b71152340
Hardik Prajapati <hardikpnsp@gmail.com>
2021-05-02 12:39:31 +0530

    ADD: Exercise 3.56

--- chapter_03/3_5_2_infinite_streams.scm
+++ chapter_03/3_5_2_infinite_streams.scm
@@ -30,3 +30,25 @@
   partial-sum-stream)
 
 (define int-sum (partial-sum integers))
+
+;; Exercise 3.56: all integers with no prime factors other than 2, 3, 5
+
+(define (merge s1 s2)
+  (cond ((stream-null? s1) s2)
+        ((stream-null? s2) s1)
+        (else
+         (let ((s1car (stream-car s1))
+               (s2car (stream-car s2)))
+           (cond ((< s1car s2car)
+                  (cons-stream s1car (merge (stream-cdr s1) s2)))
+                 ((> s1car s2car)
+                  (cons-stream s2car (merge s1 (stream-cdr s2))))
+                 (else
+                  (cons-stream s1car
+                               (merge (stream-cdr s1)
+                                      (stream-cdr s2)))))))))
+
+(define (scale-stream stream factor)
+  (stream-map (lambda (x) (* x factor)) stream))
+
+(define S (cons-stream 1 (merge (scale-stream S 2) (merge (scale-stream S 3) (scale-stream S 5)))))
